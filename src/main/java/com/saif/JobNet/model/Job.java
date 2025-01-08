8005525b2609ec1ee11d381a1c658f37eb03f2f0
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-08 23:59:16 +0530

    added Lombok in the project

--- src/main/java/com/saif/JobNet/model/Job.java
+++ src/main/java/com/saif/JobNet/model/Job.java
@@ -1,12 +1,16 @@
 package com.saif.JobNet.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Document(collection ="jobs")
+@Data //for automatic getter,setter,toString generator
 public class Job {
 
     @Id
@@ -41,102 +45,102 @@ public class Job {
 
     private LocalDateTime date;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void setSalary(String salary) {
-        this.salary = salary;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setRating(String rating) {
-        this.rating = rating;
-    }
-
-    public void setReviews(String reviews) {
-        this.reviews = reviews;
-    }
-
-    public void setPost_date(String post_date) {
-        this.post_date = post_date;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-//    public void setOpenings(String openings) {
-//        this.openings = openings;
+//    public void setId(String id) {
+//        this.id = id;
 //    }
-//    public void setApplicants(String applicants) {
-//        this.applicants = applicants;
+//
+//    public void setLocation(String location) {
+//        this.location = location;
 //    }
-
-    public String getId() {
-        return id;
-    }
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public String getSalary() {
-        return salary;
-    }
-
-//    public String getOpenings() {
-//        return openings;
+//
+//    public void setSalary(String salary) {
+//        this.salary = salary;
 //    }
-//    public String getApplicants() {
-//        return applicants;
+//
+//    public void setUrl(String url) {
+//        this.url = url;
 //    }
-    public String getUrl() {
-        return url;
-    }
-
-    public String getRating() {
-        return rating;
-    }
-    public String getPostDate() {
-        return post_date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-    public String getReviews() {
-        return reviews;
-    }
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
+//
+//    public void setRating(String rating) {
+//        this.rating = rating;
+//    }
+//
+//    public void setReviews(String reviews) {
+//        this.reviews = reviews;
+//    }
+//
+//    public void setPost_date(String post_date) {
+//        this.post_date = post_date;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public void setCompany(String company) {
+//        this.company = company;
+//    }
+//
+////    public void setOpenings(String openings) {
+////        this.openings = openings;
+////    }
+////    public void setApplicants(String applicants) {
+////        this.applicants = applicants;
+////    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public String getCompany() {
+//        return company;
+//    }
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public String getSalary() {
+//        return salary;
+//    }
+//
+////    public String getOpenings() {
+////        return openings;
+////    }
+////    public String getApplicants() {
+////        return applicants;
+////    }
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public String getRating() {
+//        return rating;
+//    }
+//    public String getPostDate() {
+//        return post_date;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//    public String getReviews() {
+//        return reviews;
+//    }
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
 
 
     @Override
