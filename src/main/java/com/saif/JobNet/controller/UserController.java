5ad0f22b44df9c405c4bb300484a70d1ee521aed
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-10 00:01:20 +0530

    implemented user controller and necessary methods for api call

--- src/main/java/com/saif/JobNet/controller/UserController.java
+++ src/main/java/com/saif/JobNet/controller/UserController.java
@@ -1,9 +1,51 @@
 package com.saif.JobNet.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.saif.JobNet.model.User;
+import com.saif.JobNet.services.UserService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUser(),HttpStatus.OK);
+    }
+
+    @GetMapping("id/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable ObjectId id){
+        User user=userService.getUserById(id);
+        if(user!=null){
+            return new ResponseEntity<>(user,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody User user){
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        User userBefore=userService.getUserById(user.getId());
+        userService.saveUser(user);
+        User userAfter =userService.getUserById(user.getId());
+        if(userService.checkUpdatedOrNot(userBefore,userAfter))
+            return new ResponseEntity<>(userAfter,HttpStatus.ACCEPTED);
+
+        return new ResponseEntity<>(userAfter,HttpStatus.NOT_MODIFIED);
+    }
+
 }
--- src/main/java/com/saif/JobNet/services/UserService.java
+++ src/main/java/com/saif/JobNet/services/UserService.java
@@ -3,11 +3,13 @@ package com.saif.JobNet.services;
 import com.saif.JobNet.model.User;
 import com.saif.JobNet.repositories.JobsRepository;
 import com.saif.JobNet.repositories.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Component
@@ -16,7 +18,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void insertUser(User user){
+    public void saveUser(User user){
         userRepository.insert(user);
     }
 
@@ -24,4 +26,25 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User getUserById(ObjectId id){
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()){
+            return user.get();
+        }
+        return null;
+    }
+
+    public void deleteUserById(ObjectId id){
+        userRepository.deleteById(id);
+    }
+
+    public boolean checkUpdatedOrNot(User userBefore, User userAfter) {
+        if(userBefore.getEmail().equals(userAfter.getEmail())
+        && userBefore.getPassword().equals(userAfter.getPassword())
+        && userBefore.getName().equals(userAfter.getName())
+        && userBefore.getPhoneNumber().equals(userAfter.getPhoneNumber())){
+            return false;
+        }
+        return true;
+    }
 }
