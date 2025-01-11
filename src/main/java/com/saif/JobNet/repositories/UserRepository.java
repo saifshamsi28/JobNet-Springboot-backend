b985f42341b90b4c724fcc109860b08ea6b6f253
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-12 01:05:45 +0530

    cleaned the code

--- src/main/java/com/saif/JobNet/repositories/UserRepository.java
+++ src/main/java/com/saif/JobNet/repositories/UserRepository.java
@@ -1,12 +1,9 @@
 package com.saif.JobNet.repositories;
 
 import com.saif.JobNet.model.User;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 }
