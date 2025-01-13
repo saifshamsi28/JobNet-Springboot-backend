39b4d67f61cfcf2346721d02d12b7301755aedd3
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-13 21:10:07 +0530

    added method to check email

--- src/main/java/com/saif/JobNet/repositories/UserRepository.java
+++ src/main/java/com/saif/JobNet/repositories/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByUserName(String userName);
+    boolean existsByEmail(String email);
 }
