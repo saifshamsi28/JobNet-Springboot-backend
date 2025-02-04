24af4a8a363bd774aca8e8a7833eb2df9bef7264
Toxic-saif <saifshamsi9905@gmail.com>
2025-02-05 01:25:29 +0530

    improved the query to partial matching from title and description

--- src/main/java/com/saif/JobNet/repositories/JobsRepository.java
+++ src/main/java/com/saif/JobNet/repositories/JobsRepository.java
@@ -3,6 +3,7 @@ package com.saif.JobNet.repositories;
 import com.saif.JobNet.model.Job;
 import org.springframework.data.domain.Example;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,8 @@ import java.util.function.Function;
 
 @Repository
 public interface JobsRepository extends MongoRepository<Job,String> {
-    List<Job> getJobByTitle(String title);
+    @Query("{ '$or': [ { 'title': { $regex: ?0, $options: 'i' } }, { 'description': { $regex: ?0, $options: 'i' } } ] }")
+    List<Job> searchJobsByTitleOrDescription(String title);
 
 //    @Override
 //    <S extends Job, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction);
