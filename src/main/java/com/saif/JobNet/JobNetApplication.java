877953ebf2c952056bf3d2297b244f24dc72124d
Toxic-saif <saifshamsi9905@gmail.com>
2025-02-16 00:18:35 +0530

    remove unnecessary code

--- src/main/java/com/saif/JobNet/JobNetApplication.java
+++ src/main/java/com/saif/JobNet/JobNetApplication.java
@@ -1,21 +1,12 @@
 package com.saif.JobNet;
 
-import com.saif.JobNet.model.Job;
-import com.saif.JobNet.repositories.JobsRepository;
-import com.saif.JobNet.services.JobsService;
-import com.saif.JobNet.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.saif.JobNet.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class JobNetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobNetApplication.class, args);
-		System.out.println("HOST: " + System.getenv("HOST"));
-		System.out.println("PORT: " + System.getenv("PORT"));
-		System.out.println("DATABASE: " + System.getenv("DATABASE"));
-
 	}
 }
