1949e8e1f9edd5a330a6fea1622c4066bc59e48b
Toxic-saif <saifshamsi9905@gmail.com>
2025-03-01 05:39:59 +0530

    created resume model to return response in meaningful manner

---
+++ src/main/java/com/saif/JobNet/model/Resume.java
@@ -0,0 +1,12 @@
+package com.saif.JobNet.model;
+
+import lombok.Data;
+
+@Data
+public class Resume {
+    private String userId;
+    private String resumeName;
+    private String resumeUrl;
+    private String resumeUploadDate;
+    private String resumeSize;
+}
