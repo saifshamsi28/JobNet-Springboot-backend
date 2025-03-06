cd67748f07bf3d15e41562d66c579c168de9a407
Toxic-saif <saifshamsi9905@gmail.com>
2025-03-06 16:37:09 +0530

    added method to get user by email or username

--- src/main/java/com/saif/JobNet/repositories/UserRepository.java
+++ src/main/java/com/saif/JobNet/repositories/UserRepository.java
@@ -4,8 +4,12 @@ import com.saif.JobNet.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByUserName(String userName);
     boolean existsByEmail(String email);
+    Optional<User> getUserByEmail(String email);
+    Optional<User> getUserByUserName(String userName);
 }
