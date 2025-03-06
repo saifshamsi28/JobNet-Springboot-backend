73e27366ad965f03ba151ff5cec9a19915cf5aad
Toxic-saif <saifshamsi9905@gmail.com>
2025-03-06 16:36:07 +0530

    remove authentication functionality from user controller

--- src/main/java/com/saif/JobNet/controller/UserController.java
+++ src/main/java/com/saif/JobNet/controller/UserController.java
@@ -42,38 +42,11 @@ public class UserController {
                 .body(available);
     }
 
-    @PostMapping("email/{email}")
-    public ResponseEntity<Boolean> checkEmailAlreadyExists(@PathVariable String email) {
-        boolean available = userService.checkEmailAlreadyExists(email);
-        System.out.println("email: " + email + ", available: " + available);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(available);
-    }
-
     @PostMapping
     public ResponseEntity<?> saveUser(@RequestBody User user){
         System.out.println("we got the user : "+user.getName()+" username: "+user.getUserName());
         userService.saveUser(user);
         return new ResponseEntity<>(user,HttpStatus.CREATED);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody UserLoginCredentials credentials) {
-        String userNameOrEmail = credentials.getUserNameOrEmail();
-        String password = credentials.getPassword();
-
-        System.out.println("username or email: "+credentials.getUserNameOrEmail());
-        System.out.println("password: "+credentials.getPassword());
-        Optional<User> userOpt = userService.getAllUser().stream()
-                .filter(user -> (user.getUserName().equals(userNameOrEmail) || user.getEmail().equals(userNameOrEmail))
-                        && user.getPassword().equals(password))
-                .findFirst();
-
-        if (userOpt.isPresent()) {
-            return new ResponseEntity<>(userOpt.get(), HttpStatus.OK); // Return user details
-        } else {
-            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED); // Invalid credentials
-        }
     }
 
     @PutMapping
@@ -135,15 +108,6 @@ public class UserController {
             response.put("message", "No user is present with id: " + id);
             return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-    }
-
-    @DeleteMapping("all")
-    public ResponseEntity<?> deleteAllUsers(){
-        userService.deleteAllUsers();
-        if(userService.getAllUser().isEmpty()){
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
     }
 
     @PutMapping("/save-jobs")
