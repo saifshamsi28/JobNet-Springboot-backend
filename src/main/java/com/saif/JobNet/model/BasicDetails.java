521716b3d24cab2b86184bd3a63364c34c772a5a
Toxic-saif <saifshamsi9905@gmail.com>
2025-03-12 23:51:42 +0530

    added basic details class

---
+++ src/main/java/com/saif/JobNet/model/BasicDetails.java
@@ -0,0 +1,11 @@
+package com.saif.JobNet.model;
+
+import lombok.Data;
+
+@Data
+public class BasicDetails {
+        private String gender;
+        private String dateOfBirth;
+        private String currentCity;
+        private String homeTown;
+}
