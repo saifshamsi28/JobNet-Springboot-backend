9eb866f9751523a99e784b0b756b45cebd6cc14f
Toxic-saif <saifshamsi9905@gmail.com>
2025-04-02 22:21:36 +0530

    removed the unused methods

--- src/main/java/com/saif/JobNet/services/UserService.java
+++ src/main/java/com/saif/JobNet/services/UserService.java
@@ -5,11 +5,7 @@ import com.saif.JobNet.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 
 @Service
@@ -28,7 +24,6 @@ public class UserService {
     }
 
     public Optional<User> getUserById(String id){
-//        System.out.println("in user service getting user by id: "+id);
         return userRepository.findById(id);
     }
 
@@ -45,19 +40,6 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
-    public void deleteAllUsers(){
-        userRepository.deleteAll();
-    }
-    public boolean checkUpdatedOrNot(User userBefore, User userAfter) {
-        if(userBefore.getEmail().equals(userAfter.getEmail())
-        && userBefore.getPassword().equals(userAfter.getPassword())
-        && userBefore.getName().equals(userAfter.getName())
-        && userBefore.getPhoneNumber().equals(userAfter.getPhoneNumber())){
-            return false;
-        }
-        return true;
-    }
-
     public boolean checkUserNameAvailable(String username){
         return userRepository.existsByUserName(username);
     }
@@ -66,21 +48,4 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 
-    public String generateUniqueFileName(String originalFileName) {
-        String extension = "";
-        int dotIndex = originalFileName.lastIndexOf(".");
-
-        if (dotIndex > 0) {
-            extension = originalFileName.substring(dotIndex);  // Get file extension
-            originalFileName = originalFileName.substring(0, dotIndex);  // Remove extension
-        }
-
-        String timestamp = String.valueOf(System.currentTimeMillis());  // Unique timestamp
-        return originalFileName + "_" + timestamp + extension;  // New unique filename
-    }
-
-    public String getCurrentDate() {
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault());
-        return sdf.format(new Date());
-    }
 }
