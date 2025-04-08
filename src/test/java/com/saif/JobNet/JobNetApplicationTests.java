0f41391a7d761599cea95ebf7668f299b4cd88f4
Toxic-saif <saifshamsi9905@gmail.com>
2025-04-08 10:41:10 +0530

    cleaned the code

--- src/test/java/com/saif/JobNet/JobNetApplicationTests.java
+++ src/test/java/com/saif/JobNet/JobNetApplicationTests.java
@@ -14,45 +14,45 @@ import java.util.Map;
 @SpringBootTest
 class JobNetApplicationTests {
 
-	@Autowired
-	private RestTemplate restTemplate;
+//	@Autowired
+//	private RestTemplate restTemplate;
 	@Test
 	void contextLoads() {
-		String url="http://10.151.17.41:5000/home?job_title=android developer";
-
-		try {
-			url = url.trim();
-
-			// Construct the Flask backend URL
-
-//			System.out.println("Sending request to Flask: " + flaskEndpoint);
-
-			// Call Flask backend using RestTemplate
-			ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
-
-			if (response.getStatusCode() == HttpStatus.OK) {
-				List<Job> responseBody = (List<Job>) response.getBody();
-				if (responseBody != null) {
-//					if (responseBody.containsKey("full_description")) {
-//						return (String) responseBody.get("full_description");
-//					} else if (responseBody.containsKey("error")) {
-//						return "Error from Flask: " + responseBody.get("error");
-//					} else {
-//						return "Unexpected response from Flask.";
-//					}
-					System.out.println(responseBody);
-				} else {
-					System.out.println( "Empty response from Flask.");
-				}
-			} else {
-				throw new RuntimeException("Failed to fetch job description from Flask: " +
-						response.getStatusCode());
-			}
-		} catch (Exception e) {
-			// Handle exceptions gracefully
-			System.err.println("Error communicating with Flask backend: " + e.getMessage());
-//			return "Unable to fetch job description due to a backend issue.";
-		}
+//		String url="http://10.151.17.41:5000/home?job_title=android developer";
+//
+//		try {
+//			url = url.trim();
+//
+//			// Construct the Flask backend URL
+//
+////			System.out.println("Sending request to Flask: " + flaskEndpoint);
+//
+//			// Call Flask backend using RestTemplate
+//			ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+//
+//			if (response.getStatusCode() == HttpStatus.OK) {
+//				List<Job> responseBody = (List<Job>) response.getBody();
+//				if (responseBody != null) {
+////					if (responseBody.containsKey("full_description")) {
+////						return (String) responseBody.get("full_description");
+////					} else if (responseBody.containsKey("error")) {
+////						return "Error from Flask: " + responseBody.get("error");
+////					} else {
+////						return "Unexpected response from Flask.";
+////					}
+//					System.out.println(responseBody);
+//				} else {
+//					System.out.println( "Empty response from Flask.");
+//				}
+//			} else {
+//				throw new RuntimeException("Failed to fetch job description from Flask: " +
+//						response.getStatusCode());
+//			}
+//		} catch (Exception e) {
+//			// Handle exceptions gracefully
+//			System.err.println("Error communicating with Flask backend: " + e.getMessage());
+////			return "Unable to fetch job description due to a backend issue.";
+//		}
 	}
 
 }
