d112435521cbd7b75b42675e5bd33fba230992c5
Toxic-saif <saifshamsi9905@gmail.com>
2025-04-12 13:38:11 +0530

    created resume parsing service to parse the resume

---
+++ src/main/java/com/saif/JobNet/services/ResumeParsingService.java
@@ -0,0 +1,47 @@
+package com.saif.JobNet.services;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class ResumeParsingService {
+
+    public static Map<String, Object> parseResumeWithFlask(File resumeFile) throws IOException {
+        // 🔁 Replace this with your actual Flask server URL
+        String flaskUrl = "http://10.162.1.53:5000/parse-resume";
+
+        HttpPost post = new HttpPost(flaskUrl);
+
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody("resume", resumeFile, ContentType.DEFAULT_BINARY, resumeFile.getName());
+
+        HttpEntity multipart = builder.build();
+        post.setEntity(multipart);
+
+        try (CloseableHttpClient client = HttpClients.createDefault();
+             CloseableHttpResponse response = client.execute(post)) {
+
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                String result = EntityUtils.toString(responseEntity);
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.readValue(result, new TypeReference<Map<String, Object>>() {});
+            } else {
+                throw new IOException("No response from Flask server");
+            }
+        }
+    }
+}
