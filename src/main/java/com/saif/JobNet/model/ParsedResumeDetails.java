8c20c7e8aaf56098e0a37befca62510e58cda88e
Toxic-saif <saifshamsi9905@gmail.com>
2025-04-12 23:41:02 +0530

    created a class to store the parsed resume details

---
+++ src/main/java/com/saif/JobNet/model/ParsedResumeDetails.java
@@ -0,0 +1,20 @@
+package com.saif.JobNet.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ParsedResumeDetails {
+        private String name;
+        private String email;
+        private String phone;
+        private String linkedin;
+        private String github;
+        private String portfolio;
+        private List<String> skills;
+        private String experience;
+        @JsonProperty("raw_text_snippet")
+        private String rawTextSnippet;
+}
